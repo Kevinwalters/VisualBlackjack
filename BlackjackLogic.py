fd677a6ede893db0f43bf701c95051ba4f78ece1
Kevin Michael Walters <kmw2168@columbia.edu>
2015-05-08 22:40:07 -0400

    Works on a real image; Uses perspective warping, corner detection, and template matching; can recognize a dealer's card from your own; hooked up to make decisions when run on an image

--- BlackjackLogic.py
+++ BlackjackLogic.py
@@ -5,45 +5,49 @@ Created on May 4, 2015
 '''
 
 from Cards import Cards
-import VisualBlackjack
 
-class MyClass(object):
+STAND = 0
+HIT = 1
+OVER = 2
+RESULTS = ["STAND", "HIT", "OVER"]
+
+class BlackjackLogic(object):
     
     def getDecision(self):
         if self.card_sum + self.num_ace > 21:
-            return VisualBlackjack.OVER
+            return STAND
         if self.card_sum > 16:
-            return VisualBlackjack.STAND
+            return STAND
         if self.card_sum < 12 and self.num_ace == 0:
-            return VisualBlackjack.HIT
+            return HIT
         if self.card_sum < 17 and self.num_ace == 0:
             if self.dealer_card in [Cards.TWO, Cards.THREE, Cards.FOUR, Cards.FIVE, Cards.SIX]:
-                return VisualBlackjack.STAND
+                return STAND
             else:
-                return VisualBlackjack.HIT
+                return HIT
         if self.num_ace == 1:
             if self.card_sum < 7:
-                return VisualBlackjack.HIT
+                return HIT
             if self.card_sum == 7 and self.dealer_card in [Cards.NINE, Cards.TEN, Cards.JACK, Cards.QUEEN, Cards.KING]:
-                return VisualBlackjack.HIT
+                return HIT
             if self.card_sum > 7:
-                return VisualBlackjack.STAND
+                return STAND
         # More than once Ace, need to first check if in a good position
         # If not, simulate a one-ace scenario recursively
         if self.num_ace > 1:
             tmp_sum = self.card_sum + 11 + self.num_ace - 1
             if tmp_sum > 16 and tmp_sum <= 21:
-                return VisualBlackjack.STAND
+                return STAND
             tmp_sum = self.card_sum + self.num_ace
             if tmp_sum > 16 and tmp_sum <= 21:
-                return VisualBlackjack.STAND
+                return STAND
             tmp_sum = self.card_sum + self.num_ace - 1
             if tmp_sum <= 21:
                 self.num_ace = 1
                 self.card_sum = tmp_sum
                 return self.getDecision()
             else:
-                return VisualBlackjack.OVER
+                return OVER
             
 
     def __init__(self, cards, dealer_card):
--- VisualBlackjack.py
+++ VisualBlackjack.py
@@ -5,48 +5,96 @@ Created on Apr 27, 2015
 '''
 
 
-from TemplateMatcher import TemplateMatcher
-from Cards import Cards
-import numpy as np
+import math
+from operator import itemgetter
+
 import cv2
+
+from Cards import Cards
+from TemplateMatcher import TemplateMatcher
+import numpy as np
+from BlackjackLogic import BlackjackLogic
+from BlackjackLogic import RESULTS
+
 
 CORNER_LEFT = 10
 CORNER_RIGHT = 70
 CORNER_TOP = 30
 CORNER_BOTTOM = 115
-STAND = 0
-HIT = 1
-OVER = 2
-RESULTS = [STAND, HIT, OVER]
-
-
-'''
-Given a card's contour, determines if it belongs to dealer based on position
-'''
-def isDealer(img, card_contour):
-    #see if above 1/2
-    pass
+HEIGHT = 0
+WIDTH = 0
 
 '''
 Given a card's contour, determines its rank
 Order of steps:
-    1. Get the four coordinates of the card in a known order
-    2. Perform a perspective transform, to get a top-down view of the card
-    3. Warp the perspective to 500x700 (cards are 5"x7")
-    4. Get the rank from the top-left corner of the card (to speed up template matching)
-    5. Use the template matcher on the corner to determine the rank
+    1. Color the entire contour area white
+    2. Use this area to get a list of possible corners, using FAST
+    3. Determine the actual card corners
+    4. Perform a persepctive transform, to get a top-down, 500x700 view of the card
+    5. Get the rank view from the top-left corner of the card (to speed up template matching)
+    6. Use the template matcher on the corner to determine the rank 
 '''
-def getRank(orig_img, img, card_contour):
-    # Get coordinates of the card in a known order
+def getRank(img, card_contour):
+    # Color the contour area white
+    white_card = np.zeros(img.shape[:2], np.uint8)
+    cv2.drawContours(white_card, [card_contour], 0, 255, -1)
+    cv2.imwrite('Output.png', white_card)
     
-    # TODO MIN AREA RECT INCORRECT BECAUSE IMG IS STILL WARPED AT THIS POINT
+    # Get all possible corners using FAST
+    fast = cv2.FastFeatureDetector()
+    fast.setBool('nonmaxSuppression', 0)
+    kp = fast.detect(white_card, None)
+    dists = []
     
+    # Get the centroid of the contour
     rect = cv2.minAreaRect(card_contour)
-    box = cv2.cv.BoxPoints(rect)
-    box = np.int0(box) # Box coordinates: [LR, LL, UL, UR]
+    rect_width = min([rect[1][0], rect[1][1]])
+    M = cv2.moments(card_contour)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
     
-    # Points to be mapped for perspective transformation
-    src_points = np.float32([box[0], box[1], box[2], box[3]])
+    # Get each possible corner's distance from the center
+    for point in kp:
+        dist = math.hypot(point.pt[0] - cx, point.pt[1] - cy)
+        dists.append((point, dist))
+    dists = sorted(dists, reverse=True, key=itemgetter(1))
+    # Determine which corners are actually corners
+    corners = []
+    corners.append(dists[0])
+    dists.pop(0)
+    for dist in dists:
+        brk = False
+        for corner in corners:
+            this_dist = math.hypot(corner[0].pt[0] - dist[0].pt[0], corner[0].pt[1] - dist[0].pt[1])
+            # Should not add - break, and don't add to corners
+            if this_dist < 0.7*rect_width:
+                brk = True
+                break
+        if not brk:
+            corners.append(dist)
+                
+    if len(corners) != 4:
+        return None, None
+    corners = [c[0] for c in corners]
+    corners = [c.pt for c in corners]
+
+    # Order of points: [LR, LL, UL, UR]
+    corners = sorted(corners, key=itemgetter(0)) # Sort on minimum x value
+    if corners[0][1] < corners[1][1]:
+        UL = corners[0]
+        LL = corners[1]
+    else:
+        UL = corners[1]
+        LL = corners[0]
+    if corners[2][1] < corners[3][1]:
+        UR = corners[2]
+        LR = corners[3]
+    else:
+        UR = corners[3]
+        LR = corners[2]
+        
+    src_points = [LR, LL, UL, UR]
+    src_points = np.float32(src_points)
     target_points = np.float32([[499, 699],[0, 699], [0, 0], [499, 0]])
     
     # Transform the image into a 500x700 top-down view
@@ -55,34 +103,57 @@ def getRank(orig_img, img, card_contour):
 
     # Perform template matching on the corner of the card
     card_corner = output[CORNER_TOP:CORNER_BOTTOM, CORNER_LEFT:CORNER_RIGHT]
-    
-    cv2.imshow('Output', card_corner)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    
     tm = TemplateMatcher()
     card_val = tm.matchTemplate(card_corner)
     print Cards.CARDS[card_val]
-    return card_val, isDealer(orig_img,card_contour)
+    # Determine if it the dealer's card, or your card
+    if cy < 0.4*HEIGHT:
+        isDealer = True
+    else:
+        isDealer = False
+    return card_val, isDealer
 
 
 if __name__ == '__main__':
     
-    img = cv2.imread("jack-and-king.png")
+    img = cv2.imread("real-image.png")
+    print img.shape
+    HEIGHT = img.shape[0]
+    WIDTH = img.shape[1]
     
-    imggray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, b_img = cv2.threshold(imggray, 127, 255, 0)
+    # A boundary for any non-green pixel
+    green_boundary = ([0, 230, 0], [255, 255, 255])
+    lower_boundary = np.array(green_boundary[0], dtype="uint8")
+    upper_boundary = np.array(green_boundary[1], dtype="uint8")
+    #apply a mask to the image, using the boundaries
+    mask = cv2.inRange(img, lower_boundary, upper_boundary)
+    no_green = cv2.bitwise_and(img, img, mask = mask)
+    kernel = np.ones((4,4), np.uint8)
+    no_green = cv2.morphologyEx(no_green, cv2.MORPH_CLOSE, kernel)
+
+    to_binary = no_green.copy()
+
+    imggray = cv2.cvtColor(to_binary, cv2.COLOR_BGR2GRAY)
+    ret, b_img = cv2.threshold(imggray, 0, 255, 0)
+    cv2.imwrite('Output.png', b_img)
+
     img2 = b_img.copy()
     cnt, _ = cv2.findContours(b_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print len(cnt)
+    
     hand = []
+    dealer = None
     for j in range(len(cnt)):
         new_img = img2.copy()
-        contour = cnt[j]
-        mask = np.zeros_like(new_img)
-        cv2.drawContours(mask, cnt, j, 255, -1)
-        out_img = np.zeros_like(new_img)
-        out_img[mask == 255] = new_img[mask == 255]
-        
-        card_val, isDealer = getRank(new_img, out_img, contour)
-        hand.append(card_val)
+        card_val, isDealer = getRank(new_img, cnt[j])
+        if not card_val and not isDealer:
+            print "Could not identify card corners. Unable to identify face card."
+            exit()       
+        #card_val, isDealer = getRank(new_img, out_img, contour)
+        if not isDealer:
+            hand.append(card_val)
+        else:
+            dealer = card_val
+    
+    blackjack = BlackjackLogic(hand, dealer)
+    decision = blackjack.getDecision()
+    print RESULTS[decision]
